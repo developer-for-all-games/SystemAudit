d3a2c33a62da700c90f93c923a8df275f19bcb9e
Developer <aydenleighbates12@gmail.com>
2026-05-20 19:20:40 -0400

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,10 +1,9 @@
-# SystemAudit Config - Store this in your private repo
 $script:AuditConfig = @{
-    EmailTo      = "aydenbates33@gmail.com"
-    EmailFrom    = "aydenbates33@gmail.com"
-    EmailPassword = "12177717Ab!!!!!"  # Gmail App Password
-    SMTPServer   = "smtp.gmail.com"
-    SMTPPort     = 587
-    ZipResults   = $true
-    OutputPath   = "C:\SystemAudit"
+    EmailTo       = "aydenbates33@gmail.com"
+    EmailFrom     = "aydenbates33@gmail.com"
+    EmailPassword = "zojq marp tuho exsg"
+    SMTPServer    = "smtp.gmail.com"
+    SMTPPort      = 587
+    ZipResults    = $true
+    OutputPath    = "C:\SystemAudit"
 }
